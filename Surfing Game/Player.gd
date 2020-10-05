2f64d5ff819f34adba44370350599a1b518596cc
Daniel-control <70458849+Daniel-control@users.noreply.github.com>
2020-10-05 17:14:23 +1100

    Patch 0.2 (Charater Movement)
    In patch 0.2, I have updated the charaters movement so that he can go on both axis.

--- Surfing Game/Player.gd
+++ Surfing Game/Player.gd
@@ -6,7 +6,14 @@ var motion = Vector2.ZERO
 
 func _physics_process(delta):
 	var y_input = Input.get_action_strength("up") - Input.get_action_strength("down")
+	var x_input = Input.get_action_strength("right") - Input.get_action_strength("left")
 	
+	if x_input != 0:
+		motion.x += x_input * Acceleration * delta
+		motion.x = clamp(motion.x, -Max_Speed, Max_Speed)
+	else:
+		motion.x = lerp(motion.x, 0, 0.06)
+		
 	if y_input != 0:
 		motion.y += y_input * Acceleration * delta
 		motion.y = clamp(motion.y, -Max_Speed, Max_Speed)
