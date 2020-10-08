2f7a824d085d4b2fc47ad81fc0e722695112111a
Daniel-control <70458849+Daniel-control@users.noreply.github.com>
2020-10-08 18:22:57 +1100

    Patch 0.5 (Fixing the Gun)

--- Surfing Game/Gun.gd
+++ Surfing Game/Gun.gd
@@ -1,10 +1,17 @@
 extends Area2D
 
-var same_gun = "no"
+var always_true = true
+var is_on_hand = false
 
 func _on_Gun_body_entered(body):
-	if body.name == "Player":
-		var same_gun = "yes"
-		while same_gun == "yes":
-			var player_pos = get_position()
-			print(player_pos)
+		if body.name == "Player":
+			while always_true == true:
+				var player_pos = get_parent().get_node("Player").position
+				set_position(player_pos)
+				is_on_hand = true
+				yield(get_tree(), "idle_frame")
+				
+func _process(delta):
+	if is_on_hand == true:
+		var mouse_pos = get_local_mouse_position()
+		rotation += mouse_pos.angle() * 0.1
